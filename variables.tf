c1dfa0570b62bbf832656161b7656123a4aef142
Mikhail Naletov <okgolove@markeloff.net>
2020-05-07 14:11:13 +0300

    Init module

---
+++ variables.tf
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2020 DevOps, SIA. - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the Apache License Version 2.0.
+ * http://www.apache.org/licenses
+ */
+
+##########
+# Naming #
+##########
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Solution name, e.g. 'app' or 'jenkins'"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+}
